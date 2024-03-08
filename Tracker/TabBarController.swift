abe8c8d8f87dd3c89b5ee96b57477ad632b2db04
a.krasnov <wonsarc@icloud.com>
2024-03-08 20:52:46 +0300

    add appIcon

---
+++ Tracker/TabBarController.swift
@@ -0,0 +1,14 @@
+//
+//  TabBarController.swift
+//  Tracker
+//
+//  Created by Artem Krasnov on 08.03.2024.
+//
+
+import UIKit
+
+final class TabBarController: UITabBarController {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
