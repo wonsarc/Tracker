1df4bf0682213652c9d99269c7f88d8abd0338c3
a.krasnov <wonsarc@icloud.com>
2024-06-12 22:28:10 +0300

    add global filter and refactoring date in done tracker + trackers VC

---
+++ Tracker/FilterViewCell.swift
@@ -0,0 +1,8 @@
+//
+//  FilterViewCell.swift
+//  Tracker
+//
+//  Created by Artem Krasnov on 12.06.2024.
+//
+
+import Foundation
---
+++ Tracker/FiltersModel.swift
@@ -0,0 +1,8 @@
+//
+//  FiltersModel.swift
+//  Tracker
+//
+//  Created by Artem Krasnov on 12.06.2024.
+//
+
+import Foundation
